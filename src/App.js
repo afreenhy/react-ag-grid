01fc9e8d5d7f6b7846c9ea99390e023ea8a4983e
Yaligar <Afreenbanu.Yaligar@rsa.com>
2019-10-25 00:55:37 +0530

    react ag grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data'
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const columns = Object.keys(data.results[0]);
+    this.state = {
+      
+      columnDefs: columns,
+      rowData: data.results
+
+    }
+  }
+
+  render() {
+    return (
+      <div 
+        className="ag-theme-balham"
+        style={{ 
+        height: '500px', 
+        width: '600px' }} 
+      >
+        <AgGridReact
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData}>
+        </AgGridReact>
+      </div>
+    );
+  }
 }
 
 export default App;
